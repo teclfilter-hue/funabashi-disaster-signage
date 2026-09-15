cd16c8017bdf4fa6f467085fa01563a200174a09
teclfilter-hue <teclfilter@gmail.com>
2026-09-15 16:12:21 +0900

    Update config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,6 +1,6 @@
 window.SIGNAGE_CONFIG = {
   // ★ Cloudflare Workersをデプロイした後、このURLに変更してください。
-  API_BASE_URL: "https://YOUR-WORKER.workers.dev",
+  API_BASE_URL: "https://funabashi-disaster-signage.teclfilter.workers.dev",
 
   AREA_NAME: "千葉県船橋市",
   AREA_CODE: "1220400",
